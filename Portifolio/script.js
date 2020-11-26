135dc097a6678fe0ca5993406c1c85544545ff29
João Hugo Leda <joaohugoleda2@gmail.com>
2020-11-26 10:12:09 -0300

    Update script.js

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -1,12 +1,9 @@
 window.onscroll = function(){myFunction()}
 
-function myFunction() {    
-    let winscroll = document.body.scrollTop || document.documentElement.scrollTop
-    
+function myFunction() {  
+    let barra = document.getElementById('mybar')  
+    let winscroll = document.body.scrollTop || document.documentElement.scrollTop    
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
     let scrolled = (winscroll / height) * 100
-    
-    let barra = document.getElementById('mybar')
-    
-    barra.style.width = scrolled + '%'
+    barra.style.width = scrolled + '%' 
 }
