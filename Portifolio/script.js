d181ed81a9bc2154df79b8176916d2c427ac0884
João Hugo Leda <joaohugoleda2@gmail.com>
2020-12-10 23:04:30 -0300

    RemoveChild

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -1,4 +1,5 @@
 window.onscroll = function(){ myFunction() }
+window.onresize = function(){ sideBarControl() }
 
 function myFunction() {  
     let barra = document.getElementById('mybar')  
@@ -6,4 +7,15 @@ function myFunction() {
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
     let scrolled = (winscroll / height) * 100
     barra.style.width = scrolled + '%' 
+}
+
+function sideBarControl() {    
+    let width =  window.innerWidth  
+    if (width < 750) {
+        var sideBar = document.getElementById('sidebar')
+        if (sideBar.parentNode) {
+            sideBar.parentNode.removeChild(sideBar)
+        }
+        alert('sdad')
+    }
 }
