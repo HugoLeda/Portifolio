27f43f75a2f1d80953aecad9db9ee8afdad5276c
João Hugo Leda <joaohugoleda2@gmail.com>
2021-01-04 20:23:10 -0300

    04-01

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -1,15 +1,16 @@
 window.onscroll = function(){ myFunction() }
 window.onresize = function(){ sideBarControl() }
+var teste = 1
 
-function myFunction() {  
+function myFunction() {      
     let barra = document.getElementById('mybar')  
     let winscroll = document.body.scrollTop || document.documentElement.scrollTop    
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
     let scrolled = (winscroll / height) * 100
-    barra.style.width = scrolled + '%' 
+    barra.style.width = scrolled + '%'     
 }
 
-function sideBarControl() {    
+function sideBarControl() {        
     let width =  window.innerWidth  
     if (width < 650) {
         var sideBar = document.getElementById('sidebar')
@@ -18,12 +19,14 @@ function sideBarControl() {
         }        
     } else {
         window.location.reload(true)
-    }
+    }    
 }
 
 function openMenu(){
+    /*
     let input = window.document.querySelector('.check') 
-    if (input.checked) {
-        alert('gtd6sf')
-    }
+    input.checked = true;    
+    */
+    let menuMobile = document.querySelector('.menu-mobile')
+    menuMobile.style.right = 0    
 }
